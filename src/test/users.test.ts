806e2c3e430be960798a866ba32b11fc75e55ed8
Brayan Gamboa <bsgv2005@gmail.com>
2022-06-15 01:28:49 -0500

    Test - Carga 2

--- src/test/users.test.ts
+++ src/test/users.test.ts
@@ -1,7 +1,8 @@
-import mongodb from 'mongodb';
 const { app, server } = require("../index");
 import Request from 'supertest';
-const id = 33
+
+// Por medio de esta constante podemos hacer las pruebas de nuestra API
+let id_user = 33, id_company = 10015;
 
 
 afterAll(() => {
@@ -15,7 +16,7 @@ describe("Users", () => {
         expect(response.status).toBe(201);
     });
 
-    test.skip("Post - New User", async () => {
+    test("Post - New User", async () => {
         const response = await Request(app).post("/postUser").send({
             email: "mariocardenas@gmail.com",
             document: 420106835,
@@ -31,7 +32,7 @@ describe("Users", () => {
     });
 
     test("Put - Update Info User", async () => {
-        const response = await Request(app).put(`/putUser/${id}`).send({
+        const response = await Request(app).put(`/putUser/${id_user}`).send({
             email: "mariocardenas@gmail.com",
             document: 420106835,
             name: "Prueba",
@@ -39,37 +40,67 @@ describe("Users", () => {
             phone: "3136763849"
         });
         expect(response.status).toBe(201);
-        console.log(response);
-
-
     });
 
-
-
     test("Patch - Update Info User", async () => {
-        const response = await Request(app).patch("/patchUser/11").send({
+        const response = await Request(app).patch(`/patchUser/${id_user}`).send({
             phone: "3208763849",
         });
         expect(response.status).toBe(201);
-
     });
 
     test("Delete - Delete User", async () => {
-        const response = await Request(app).delete("/deleteUser/11").send({
-            phone: "3208763849",
+        const response = await Request(app).delete(`/deleteUser/${id_user}`);
+        expect(response.status).toBe(201);
+    });
+});
+
+describe("Company", () => {
+    test("Get - Company", async () => {
+        const response = await Request(app).get("/allCompanies");
+        expect(response.status).toBe(201);
+    });
+
+    test("Post - New Company", async () => {
+        const response = await Request(app).post("/postCompany").send({
+            email_company: "nike123@gmail.com",
+            name_company: "Nike Inc.",
+            phone_company: "3456789123",
+            city: "Cali",
+            neighborhood: "Santa Rita",
+            streat: "Calle 102",
+            career: "Carrera 129",
+            close_time_company: "21:00:00"
         });
         expect(response.status).toBe(201);
-
     });
 
 
+    test("Put - Update Info User", async () => {
+        const response = await Request(app).put(`/putCompany/${id_company}`).send({
+            email_company: "amaretto123@gmail.com",
+            name_company: "Amaretto Inc.",
+            phone_company: "3456789123",
+            city: "Cali",
+            neighborhood: "Santa Rita",
+            streat: "Calle 102",
+            career: "Carrera 129",
+            close_time_company: "21:00:00"
+        });
+        expect(response.status).toBe(201);
+    });
+
+    test.skip("Patch - Update Info User", async () => {
+        const response = await Request(app).patch(`/patchCompany/${id_company}`).send({
+            phone_company: "3104778549",
+            city: "Medellín",
+        });
+        expect(response.status).toBe(201);
+    });
+
+    test("Delete - Delete User", async () => {
+        const response = await Request(app).delete(`/deleteCompany/${id_company}`);
+        expect(response.status).toBe(201);
+    });
+
 })
-
-// describe("Company", () => {
-
-//     test("Get - Company", async () => {
-//         const response = await Request(app).get("/allCompanies");
-//         expect(response.status).toBe(201);
-//     });
-
-// })
