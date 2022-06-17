fb77dd1acc96b9cf94902cddbe9640d67eaaa91a
David Giraldo <david.girald0@hotmail.com>
2022-06-16 19:52:25 -0500

    Correción servicio validateUser

--- src/controllers/firebase/validate_user.controller.ts
+++ src/controllers/firebase/validate_user.controller.ts
@@ -1,22 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import { pool } from '../../data-base/config.postgres';
 import { QueryResult } from 'pg';
-import { atob } from "buffer";
+import jwt_decode from "jwt-decode";
 
 
-export const validateUser = async (_req: Request, res: Response, next: NextFunction) => {
+
+export const validateUser = async (_req: Request, res: Response) => {
     let cliente = await pool.connect();
+    const token = _req.headers.authorization?.split(" ")[1];
+    let  decoded = jwt_decode(token);
+    let emailToken = (Object.values(decoded))[7];
+    // console.log((Object.values(decoded))[7]);
+    console.log(emailToken);
     try{
-        const token = _req.headers.authorization?.split(" ")[1];
-        let base64Url = token.split('.')[1];
-        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-        const emailToken = JSON.parse(jsonPayload).email;
         let result: QueryResult = await cliente.query('SELECT * FROM users WHERE email=$1;', [emailToken]);
         if(result.rows.length == 0){
-            res.status(401).json([{message: "Usuario registrado en Firebase, pero no existe en la base de datos"}, {email: JSON.parse(jsonPayload).email}]);
+            res.status(401).json([{message: "Usuario registrado en Firebase, pero no existe en la base de datos"}, {email: emailToken}]);
         } else{
             res.status(201).json(result.rows);
         }
